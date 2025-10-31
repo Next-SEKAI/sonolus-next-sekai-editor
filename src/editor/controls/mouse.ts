1456f0ba23efd3caf4a95c1e6a11a43b51444334
Burrito <burritoyangster@gmail.com>
2025-10-31 22:46:59 +0800

    Fixed horizontal scroll direction

--- src/editor/controls/mouse.ts
+++ src/editor/controls/mouse.ts
@@ -115,11 +115,11 @@ const wheel = (event: WheelEvent) => {
         if (event.shiftKey) {
             switch (event.deltaMode) {
                 case WheelEvent.DOM_DELTA_PIXEL:
-                    scrollViewXBy(-event.deltaY, settings.mouseSmoothScrolling)
+                    scrollViewXBy(event.deltaY, settings.mouseSmoothScrolling)
                     scrollViewYBy(-event.deltaX, settings.mouseSmoothScrolling)
                     break
                 case WheelEvent.DOM_DELTA_LINE:
-                    scrollViewXBy(-(event.deltaY * 20), settings.mouseSmoothScrolling)
+                    scrollViewXBy(event.deltaY * 20, settings.mouseSmoothScrolling)
                     scrollViewYBy(-(event.deltaX * 20), settings.mouseSmoothScrolling)
                     break
                 case WheelEvent.DOM_DELTA_PAGE:
@@ -130,15 +130,15 @@ const wheel = (event: WheelEvent) => {
         } else {
             switch (event.deltaMode) {
                 case WheelEvent.DOM_DELTA_PIXEL:
-                    scrollViewXBy(-event.deltaX, settings.mouseSmoothScrolling)
+                    scrollViewXBy(event.deltaX, settings.mouseSmoothScrolling)
                     scrollViewYBy(-event.deltaY, settings.mouseSmoothScrolling)
                     break
                 case WheelEvent.DOM_DELTA_LINE:
-                    scrollViewXBy(-(event.deltaX * 20), settings.mouseSmoothScrolling)
+                    scrollViewXBy(event.deltaX * 20, settings.mouseSmoothScrolling)
                     scrollViewYBy(-(event.deltaY * 20), settings.mouseSmoothScrolling)
                     break
                 case WheelEvent.DOM_DELTA_PAGE:
-                    scrollViewXBy(-event.deltaX * view.w, settings.mouseSmoothScrolling)
+                    scrollViewXBy(event.deltaX * view.w, settings.mouseSmoothScrolling)
                     scrollViewYBy(-event.deltaY * view.h, settings.mouseSmoothScrolling)
                     break
             }
