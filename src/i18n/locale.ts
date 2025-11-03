1871ebc1db31c9d336c8c22522555915aef8bf41
Burrito <burritoyangster@gmail.com>
2025-11-03 22:18:19 +0800

    Added zht language detection

--- src/i18n/locale.ts
+++ src/i18n/locale.ts
@@ -1,5 +1,5 @@
 export const defaultLocale = (() => {
-    const [main] = navigator.language.toLowerCase().split('-')
+    const [main, ...rest] = navigator.language.toLowerCase().split('-')
 
     // eslint-disable-next-line @typescript-eslint/switch-exhaustiveness-check
     switch (main) {
@@ -8,7 +8,7 @@ export const defaultLocale = (() => {
         case 'tr':
             return main
         case 'zh':
-            return 'zhs'
+            return ['hant', 'hk', 'tw'].some((tag) => rest.includes(tag)) ? 'zht' : 'zhs'
         default:
             return 'en'
     }
