566da63baf1cc5b920354664d8f2caeefd3599ea
Burrito <burritoyangster@gmail.com>
2025-11-09 08:47:56 +0800

    Improved note tool icon

---
+++ src/editor/commands/note/icon/body/index.ts
@@ -0,0 +1,23 @@
+import DamageBody from './DamageBody.vue'
+import NoneBody from './NoneBody.vue'
+import SingleCyanBody from './SingleCyanBody.vue'
+import SingleRedBody from './SingleRedBody.vue'
+import SingleYellowBody from './SingleYellowBody.vue'
+import TraceGreenBody from './TraceGreenBody.vue'
+import TraceRedBody from './TraceRedBody.vue'
+import TraceYellowBody from './TraceYellowBody.vue'
+
+export const bodyComponents = {
+    none: NoneBody,
+    damage: DamageBody,
+    trace: {
+        red: TraceRedBody,
+        green: TraceGreenBody,
+        yellow: TraceYellowBody,
+    },
+    single: {
+        red: SingleRedBody,
+        yellow: SingleYellowBody,
+        cyan: SingleCyanBody,
+    },
+}
---
+++ src/editor/commands/note/icon/index.ts
@@ -0,0 +1,13 @@
+import AnchorIcon from './AnchorIcon.vue'
+import DamageIcon from './DamageIcon.vue'
+import SingleIcon from './SingleIcon.vue'
+import TickIcon from './TickIcon.vue'
+import TraceIcon from './TraceIcon.vue'
+
+export const iconComponents = {
+    anchor: AnchorIcon,
+    damage: DamageIcon,
+    trace: TraceIcon,
+    single: SingleIcon,
+    tick: TickIcon,
+}
