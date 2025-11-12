f9846309d2a2e607242ad0a07f148b4a5a2f3cd4
Burrito <burritoyangster@gmail.com>
2025-11-12 15:12:29 +0800

    Add sus support (#65)

--- src/editor/commands/open/index.ts
+++ src/editor/commands/open/index.ts
@@ -1,6 +1,7 @@
 import { ungzip } from 'pako'
 import type { Command } from '..'
 import { parseLevelDataChart } from '../../../chart/parse/levelData'
+import { parseSusChart } from '../../../chart/parse/sus'
 import { parseUscChart } from '../../../chart/parse/usc'
 import { validateChart } from '../../../chart/validate'
 import { checkState, resetState } from '../../../history'
@@ -8,6 +9,7 @@ import { i18n } from '../../../i18n'
 import { parseLevelData } from '../../../levelData/parse'
 import { showModal } from '../../../modals'
 import LoadingModal from '../../../modals/LoadingModal.vue'
+import { parseSus } from '../../../sus/parse'
 import { parseUsc } from '../../../usc/parse'
 import { getFilename, pickFileForOpen } from '../../../utils/file'
 import { timeout } from '../../../utils/promise'
@@ -56,6 +58,15 @@ export const open: Command = {
                         resetState(chart, usc.offset, getFilename(file))
                         break
                     }
+                    case 'sus': {
+                        const sus = parseSus(data)
+
+                        const chart = parseSusChart(sus)
+                        validateChart(chart)
+
+                        resetState(chart, sus.offset, getFilename(file))
+                        break
+                    }
                 }
 
                 notify(() => i18n.value.commands.open.opened)
@@ -71,6 +82,9 @@ const tryImport = (buffer: ArrayBuffer) => {
     const usc = tryImportUsc(buffer)
     if (usc) return ['usc', usc] as const
 
+    const sus = tryImportSus(buffer)
+    if (sus) return ['sus', sus] as const
+
     throw new Error('Unsupported file format')
 }
 
@@ -85,6 +99,17 @@ const tryImportLevelData = (buffer: ArrayBuffer): unknown => {
 const tryImportUsc = (buffer: ArrayBuffer): unknown => {
     try {
         return JSON.parse(new TextDecoder().decode(buffer))
+    } catch {
+        return
+    }
+}
+
+const tryImportSus = (buffer: ArrayBuffer) => {
+    try {
+        const data = new TextDecoder().decode(buffer).split('\n')
+        if (data.filter((line) => line.startsWith('#')).length < 10) return
+
+        return data
     } catch {
         return
     }
