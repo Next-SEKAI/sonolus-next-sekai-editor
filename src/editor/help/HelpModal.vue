7edb58b942256407f484b39b38f00c385593e790
Burrito <burritoyangster@gmail.com>
2026-04-11 05:03:23 +0800

    Add three finger tap to play (#98)

--- src/editor/help/HelpModal.vue
+++ src/editor/help/HelpModal.vue
@@ -15,6 +15,7 @@ import HelpSection from './HelpSection.vue'
             <li>{{ i18n.help.modal.touchscreen.scrollX }}</li>
             <li>{{ i18n.help.modal.touchscreen.zoomY }}</li>
             <li>{{ i18n.help.modal.touchscreen.zoomX }}</li>
+            <li>{{ i18n.help.modal.touchscreen.play }}</li>
         </HelpSection>
 
         <HelpSection :title="i18n.help.modal.mouse.title">
