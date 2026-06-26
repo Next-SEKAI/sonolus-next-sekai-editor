3d88b5ac1f208e48bbea182f685066178ca70eb3
NonSpicyBurrito <burritoyangster@gmail.com>
2026-06-26 22:54:22 +0800

    Updated packages

--- src/chart/parse/levelData/stage.ts
+++ src/chart/parse/levelData/stage.ts
@@ -1,4 +1,4 @@
-import { Type } from '@sinclair/typebox'
+import Type from 'typebox'
 import { getOptionalRef, getOptionalValue, getValue, type ParseCtx } from '.'
 import type { StageId } from '../../stages'
 
