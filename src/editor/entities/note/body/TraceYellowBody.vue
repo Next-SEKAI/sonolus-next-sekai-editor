df112ce296acfb429e3d5fc695b368e8ff45e6e3
Burrito <burritoyangster@gmail.com>
2026-07-17 04:55:28 +0800

    Improve 0 width note display (#205)

--- src/editor/entities/note/body/TraceYellowBody.vue
+++ src/editor/entities/note/body/TraceYellowBody.vue
@@ -5,5 +5,10 @@ defineProps<{
 </script>
 
 <template>
-    <rect x="0" y="0.15" :width="size" height="0.3" rx="0.1" ry="0.1" fill="#fddd86" />
+    <template v-if="size > 0">
+        <rect x="0" y="0.15" :width="size" height="0.3" rx="0.1" ry="0.1" fill="#fddd86" />
+    </template>
+    <template v-else>
+        <rect x="-0.1" y="0.15" width="0.2" height="0.3" rx="0.1" ry="0.1" fill="#fddd86" />
+    </template>
 </template>
