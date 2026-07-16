c6426727622bf6dc75fc4e2dbb8fb67e1dc796ad
NonSpicyBurrito <burritoyangster@gmail.com>
2026-07-17 04:52:14 +0800

    Improved 0 width note display

--- src/editor/entities/note/body/NoneBody.vue
+++ src/editor/entities/note/body/NoneBody.vue
@@ -8,14 +8,12 @@ defineProps<{
 </script>
 
 <template>
-    <rect
-        v-if="isHighlighted || isViewRecentlyActive"
-        x="0"
-        y="0.15"
-        :width="size"
-        height="0.3"
-        rx="0.1"
-        ry="0.1"
-        stroke="#fff"
-    />
+    <template v-if="isHighlighted || isViewRecentlyActive">
+        <template v-if="size > 0">
+            <rect x="0" y="0.15" :width="size" height="0.3" rx="0.1" ry="0.1" stroke="#fff" />
+        </template>
+        <template v-else>
+            <rect x="-0.1" y="0.15" width="0.2" height="0.3" rx="0.1" ry="0.1" stroke="#fff" />
+        </template>
+    </template>
 </template>
--- src/editor/entities/note/body/TraceRedBody.vue
+++ src/editor/entities/note/body/TraceRedBody.vue
@@ -5,5 +5,10 @@ defineProps<{
 </script>
 
 <template>
-    <rect x="0" y="0.15" :width="size" height="0.3" rx="0.1" ry="0.1" fill="#fe9ccb" />
+    <template v-if="size > 0">
+        <rect x="0" y="0.15" :width="size" height="0.3" rx="0.1" ry="0.1" fill="#fe9ccb" />
+    </template>
+    <template v-else>
+        <rect x="-0.1" y="0.15" width="0.2" height="0.3" rx="0.1" ry="0.1" fill="#fe9ccb" />
+    </template>
 </template>
